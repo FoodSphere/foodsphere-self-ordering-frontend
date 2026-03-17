abff0d68b5412681eb92f6522009e3880751ddd2
Thanathat Pinthu <thanathatp2003@gmail.com>
2026-03-18 00:41:21 +0700

    fix(payment/success/page): force dynamic to useParams

--- app/(main)/payment/success/page.tsx
+++ app/(main)/payment/success/page.tsx
@@ -1,5 +1,7 @@
 import PaymentSuccessRender from "@/app/features/main/payment/success/Index";
 
+export const dynamic = "force-dynamic";
+
 export default function PaymentSuccessPage() {
   return (
     <div className="min-h-screen flex justify-center items-center">
